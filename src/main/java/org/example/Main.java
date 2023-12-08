ab777521a0b3758ab859a4d6b39fd9cef9e5b77f
wintersky2 <emsw747@gmail.com>
2023-12-08 18:52:59 +0900

    로그인 구현 전

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,8 +10,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         List<WiseSaying> wiseSayingList = new ArrayList<>();
-
+        List<Member> memberList = new ArrayList<>();
         int idCount = 1;
+        int memberLastId = 1;
 
         while (true) {
             System.out.print("명령) ");
@@ -69,6 +70,34 @@ public class Main {
             } else if (command.equals("종료")) {
                 System.out.println("종료");
                 break;
+            } else if (command.equals("회원가입")) {
+                System.out.println("회원가입");
+
+                System.out.println("아이디 입력) ");
+                String memberId = sc.nextLine();
+
+                System.out.println("비밀번호 입력) ");
+                String password = sc.nextLine();
+                while (true) {
+                    System.out.println("비밀번호 확인) ");
+                    String passwordConfirm = sc.nextLine();
+                    if (password.equals(passwordConfirm)) {
+                        break;
+                    }else {
+                        System.out.println("비밀번호 확인 불일치");
+                    }
+                }
+
+
+                System.out.println("닉네임 입력) ");
+                String nickname = sc.nextLine();
+
+                Member member = new Member(memberLastId, memberId, password, nickname);
+                memberList.add(member);
+
+                memberLastId++;
+            } else if (command.equals("로그인")) {
+                System.out.println("아이디 입력");
             }
         }
     }
